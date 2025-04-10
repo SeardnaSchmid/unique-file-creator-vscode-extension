944bff5fb2f2d379ad5724fe9a2ac23ec2e099d8
SeardnaSchmid <as@as.as>
2025-04-10 19:00:17 +0200

    feat: add command to create file without title input and refactor file creation logic

--- src/extension.ts
+++ src/extension.ts
@@ -3,20 +3,25 @@ import fs from 'fs';
 import path from 'path';
 import moment from 'moment';
 
-// Constants for default configuration values
-const DEFAULT_TIMESTAMP_FORMAT = 'YYYY-MM-DD-HH-mm-ss ';
-const DEFAULT_POSTFIX = '';
-const DEFAULT_EXTENSION = '.md';
 
 /**
- * Generates a unique file name based on the provided parameters.
- * @param timestampFormat Format for the timestamp.
- * @param userInputTitle Title provided by the user.
- * @param postFix Postfix to append to the file name.
- * @param fileExtension File extension.
- * @param counter Optional counter for duplicate file names.
- * @returns Generated file name.
+ * Retrieves configuration values for the extension.
+ * @returns Object containing configuration values.
  */
+function getConfiguration() {
+    const config = vscode.workspace.getConfiguration('uniqueFileCreator');
+    // Constants for default configuration values
+    const DEFAULT_TIMESTAMP_FORMAT = 'YYYY-MM-DD-HH-mm-ss ';
+    const DEFAULT_POSTFIX = '';
+    const DEFAULT_EXTENSION = '.md';
+
+    return {
+        timestampFormat: config.get<string>('defaults.timestampPrefix', DEFAULT_TIMESTAMP_FORMAT),
+        postFix: config.get<string>('defaults.postFix', DEFAULT_POSTFIX),
+        fileExtension: config.get<string>('defaults.extension', DEFAULT_EXTENSION),
+    };
+}
+
 function generateFileName(
     timestampFormat: string,
     userInputTitle: string | null,
@@ -25,48 +30,49 @@ function generateFileName(
     counter: number | null = null
 ): string {
     const timestamp = moment().format(timestampFormat);
-    return `${timestamp}${userInputTitle ? userInputTitle : ""}${postFix}${counter ? counter : ''}${fileExtension}`;
-}
-
-/**
- * Retrieves configuration values for the extension.
- * @returns Object containing configuration values.
- */
-function getConfiguration() {
-    const config = vscode.workspace.getConfiguration('uniqueFileCreator');
-    return {
-        timestampFormat: config.get<string>('defaults.timestampPrefix', DEFAULT_TIMESTAMP_FORMAT),
-        postFix: config.get<string>('defaults.postFix', DEFAULT_POSTFIX),
-        fileExtension: config.get<string>('defaults.extension', DEFAULT_EXTENSION),
-    };
+    return `${timestamp}${userInputTitle ? userInputTitle : ""}${postFix}${counter ? `-${counter}` : ''}${fileExtension}`;
 }
 
 /**
  * Creates a new file in the workspace folder.
- * @param workspacePath Path to the workspace folder.
+ * @param targetFileFolder Path to the workspace folder.
  * @param fileName Initial file name.
  * @returns Path to the created file.
  */
-function createUniqueFile(workspacePath: string, fileName: string): string {
-    let filePath = path.join(workspacePath, fileName);
+function createUniqueFile(
+    timestampFormat: string,
+    fileName: string | null,
+    postFix: string,
+    fileExtension: string,
+    targetFileFolder: string
+): string {
+    let uniqueFileName = generateFileName(
+        timestampFormat,
+        fileName,
+        postFix,
+        fileExtension
+    );
+
+    let filePath = path.join(targetFileFolder, uniqueFileName);
     let counter = 1;
 
     // Ensure the file name is unique
     while (fs.existsSync(filePath)) {
-        fileName = generateFileName(
-            DEFAULT_TIMESTAMP_FORMAT,
+        // generate new targetFileName with counter prefix
+        const newFileName = generateFileName(
+            timestampFormat,
             fileName,
-            DEFAULT_POSTFIX,
-            DEFAULT_EXTENSION,
+            postFix,
+            fileExtension,
             counter
         );
-        filePath = path.join(workspacePath, fileName);
+        filePath = path.join(targetFileFolder, newFileName);
         counter++;
     }
 
     // Create the file
     fs.writeFileSync(filePath, '');
-    return filePath;
+    return targetFileFolder;
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -85,12 +91,6 @@ export function activate(context: vscode.ExtensionContext) {
                     placeHolder: 'e.g., my-new-file',
                 })) || null;
 
-                const initialFileName = generateFileName(
-                    timestampFormat,
-                    userInputTitle,
-                    postFix,
-                    fileExtension
-                );
 
                 // Get the workspace folder
                 const workspaceFolders = vscode.workspace.workspaceFolders;
@@ -99,10 +99,15 @@ export function activate(context: vscode.ExtensionContext) {
                     return;
                 }
 
-                const workspacePath = workspaceFolders[0].uri.fsPath;
+                const fileTargetPath = workspaceFolders[0].uri.fsPath;
 
                 // Create the file
-                const filePath = createUniqueFile(workspacePath, initialFileName);
+                const filePath = createUniqueFile(
+                    timestampFormat,
+                    userInputTitle,
+                    postFix,
+                    fileExtension,
+                    fileTargetPath);
 
                 // Open the file in the editor
                 const document = await vscode.workspace.openTextDocument(filePath);
@@ -115,7 +120,42 @@ export function activate(context: vscode.ExtensionContext) {
         }
     );
 
-    context.subscriptions.push(createFileCommand);
+    const createFileNoInputCommand = vscode.commands.registerCommand(
+        'unique-file-creator-vscode-extension.createFileNoInput',
+        async () => {
+            try {
+                // Get configuration values
+                const { timestampFormat, postFix, fileExtension } = getConfiguration();
+
+                // Get the workspace folder
+                const workspaceFolders = vscode.workspace.workspaceFolders;
+                if (!workspaceFolders || workspaceFolders.length === 0) {
+                    vscode.window.showErrorMessage('No workspace folder is open.');
+                    return;
+                }
+
+                const fileTargetPath = workspaceFolders[0].uri.fsPath;
+
+                // Create the file
+                const filePath = createUniqueFile(
+                    timestampFormat,
+                    null,
+                    postFix,
+                    fileExtension,
+                    fileTargetPath);
+
+                // Open the file in the editor
+                const document = await vscode.workspace.openTextDocument(filePath);
+                await vscode.window.showTextDocument(document);
+
+                vscode.window.showInformationMessage(`File created: ${filePath}`);
+            } catch (error) {
+                vscode.window.showErrorMessage(`Error creating file: ${error}`);
+            }
+        }
+    );
+
+    context.subscriptions.push(createFileCommand, createFileNoInputCommand);
 }
 
 export function deactivate() { }
